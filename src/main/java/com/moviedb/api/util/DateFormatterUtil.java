6abac18bc088f1cde9f4ce26258957da8e4e68c7
gidbalugo <zidgyril@gmail.com>
2021-03-07 15:55:36 +0800

    util class to convert string date input to LocalDateTime

--- src/main/java/com/moviedb/api/util/DateFormatterUtil.java
+++ src/main/java/com/moviedb/api/util/DateFormatterUtil.java
@@ -1,2 +1,18 @@
-package com.moviedb.api.util;public class DateFormatterUtil {
+package com.moviedb.api.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatterUtil {
+
+  private static String DEFAULT_STRING_DATE_FORMAT = "yyyy-MM-dd";
+
+  public static LocalDateTime dateFormatter(String date) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DEFAULT_STRING_DATE_FORMAT);
+    LocalDate ld = LocalDate.parse(date,formatter);
+    return LocalDateTime.of(ld, LocalDateTime.now().toLocalTime());
+  }
+
+
 }
