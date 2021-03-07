84fab836674766baa7ea681f8172c6d4ff92999c
gidbalugo <zidgyril@gmail.com>
2021-03-07 15:57:30 +0800

    movie repository as the class to communicate to database or data store

--- src/main/java/com/moviedb/api/repository/MovieRepository.java
+++ src/main/java/com/moviedb/api/repository/MovieRepository.java
@@ -1,2 +1,14 @@
-package com.moviedb.api.repository;public interface MovieRepository {
+package com.moviedb.api.repository;
+
+import com.moviedb.api.entity.Movie;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MovieRepository extends PagingAndSortingRepository<Movie, UUID> {
+
+  Optional<Movie> findById(UUID id);
+
 }
