d1a3ff864c106a9259f870938218ecb09996700c
gidbalugo <zidgyril@gmail.com>
2021-03-07 15:59:25 +0800

    temporary dto class for saving movie

--- src/main/java/com/moviedb/api/controller/MovieDto.java
+++ src/main/java/com/moviedb/api/controller/MovieDto.java
@@ -1,2 +1,15 @@
-package com.moviedb.api.controller;public class MovieDto {
+package com.moviedb.api.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MovieDto {
+
+  private String title;
+  private String overview;
+  private String poster;
+  private Integer voteAverage;
+  private String releaseDate;
 }
