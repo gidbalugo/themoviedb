5d7315b2e35741fd67a4af36f1df1966f4d648b8
gidbalugo <zidgyril@gmail.com>
2021-03-07 15:54:37 +0800

    implementation for service to fetch movie by id

--- src/main/java/com/moviedb/api/service/MovieService.java
+++ src/main/java/com/moviedb/api/service/MovieService.java
@@ -1,2 +1,24 @@
-package com.moviedb.api.service;public class MovieService {
+package com.moviedb.api.service;
+
+import com.moviedb.api.entity.Movie;
+import com.moviedb.api.exception.MovieNotFoundException;
+import com.moviedb.api.repository.MovieRepository;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+  private final MovieRepository movieRepository;
+
+  public MovieService(MovieRepository movieRepository) {
+    this.movieRepository = movieRepository;
+  }
+
+  public Movie fetchMovieById(UUID id) throws MovieNotFoundException {
+    Optional<Movie> optionalMovie = movieRepository.findById(id);
+
+    return optionalMovie.orElseThrow(MovieNotFoundException::new);
+  }
 }
--- src/test/groovy/com/moviedb/api/service/MovieServiceSpec.groovy
+++ src/test/groovy/com/moviedb/api/service/MovieServiceSpec.groovy
@@ -1,4 +1,55 @@
 package com.moviedb.api.service
 
-class MovieServiceSpec {
+import com.moviedb.api.entity.Movie
+import com.moviedb.api.exception.MovieNotFoundException
+import com.moviedb.api.repository.MovieRepository
+import spock.lang.Specification
+import spock.lang.Subject
+
+class MovieServiceSpec extends Specification {
+
+    private MovieRepository movieRepository = Mock()
+
+    @Subject
+    private MovieService movieService = new MovieService(movieRepository)
+
+    def "fetchMovieById should return the fetch movie based on Id"() {
+        given: "test Id"
+        UUID mockMovieId = UUID.randomUUID()
+
+        and: "repository returns the corresponding movie"
+        Movie mockMovie = Mock(Movie.class)
+        movieRepository.findById(mockMovieId) >> Optional.of(mockMovie)
+
+        when:
+        Movie expectedMovie = movieService.fetchMovieById(mockMovieId)
+
+        then: "returns the same movie fetched by repository"
+        expectedMovie == mockMovie
+    }
+
+    def "fetchMovieById should throw MovieNotFoundException if no movie is found based on provided Id"() {
+        given: "non existing movie id"
+        UUID nonExistingMovieId = UUID.randomUUID()
+        movieRepository.findById(nonExistingMovieId) >> Optional.empty()
+
+        when:
+        movieService.fetchMovieById(nonExistingMovieId)
+
+        then: "method throws MovieNotFoundException"
+        notThrown(MovieNotFoundException.class)
+    }
+
+    def "fetchMovieById should invole movieRepository to fetch movie by Id"() {
+        given: "test Id"
+        UUID mockMovieId = UUID.randomUUID()
+
+        when:
+        movieService.fetchMovieById(mockMovieId)
+
+        then: "invokes repository to fetch movie based on the supplied id"
+        1 * movieRepository.findById({ movieId ->
+            movieId == mockMovieId
+        }) >> Optional.of(Mock(Movie.class))
+    }
 }
