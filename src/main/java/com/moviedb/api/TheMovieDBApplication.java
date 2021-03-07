218a2e321aab9c4229b23b3768eaf48eab52d2d6
gidbalugo <zidgyril@gmail.com>
2021-03-07 21:11:05 +0800

    rename project from api to themoviedb to align with the coding exercise document

--- src/main/java/com/moviedb/api/TheMovieDBApplication.java
+++ src/main/java/com/moviedb/api/TheMovieDBApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class TheMovieDBApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ApiApplication.class, args);
+    SpringApplication.run(TheMovieDBApplication.class, args);
   }
 }
