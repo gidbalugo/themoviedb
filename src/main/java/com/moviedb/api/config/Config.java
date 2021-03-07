ee89c28d69e3abe332117c645b9a378fcd287e40
gidbalugo <zidgyril@gmail.com>
2021-03-07 15:59:01 +0800

    configuration class for auto resolver of pageable parameters

--- src/main/java/com/moviedb/api/config/Config.java
+++ src/main/java/com/moviedb/api/config/Config.java
@@ -1,5 +1,22 @@
 package com.moviedb.api.config;
 
-public class Config {
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
+  }
+
+  @Bean
+  public PageableHandlerMethodArgumentResolver pageableResolver() {
+    return new PageableHandlerMethodArgumentResolver();
+  }
 }
