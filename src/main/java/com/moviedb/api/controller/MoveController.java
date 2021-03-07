bc2a3a239a99de5ff9257e9957438280541f4022
gidbalugo <zidgyril@gmail.com>
2021-03-07 15:56:39 +0800

    initial implementation of endpoints for fetching movie by id and listing paginated movies, and a test endpoin to save movies for easier testing

--- src/main/java/com/moviedb/api/controller/MoveController.java
+++ src/main/java/com/moviedb/api/controller/MoveController.java
@@ -1,2 +1,50 @@
-package com.moviedb.api.controller;public class MoveController {
+package com.moviedb.api.controller;
+
+import com.moviedb.api.entity.Movie;
+import com.moviedb.api.exception.MovieNotFoundException;
+import com.moviedb.api.service.MovieService;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MoveController implements MovieOperations {
+
+  private final MovieService movieService;
+
+  public MoveController(MovieService movieService) {
+    this.movieService = movieService;
+  }
+
+  @Override
+  @ResponseBody
+  public ResponseEntity<Movie> getById(String id) {
+
+    try {
+      Movie movie = movieService.fetchMovieById(UUID.fromString(id));
+      return ResponseEntity.ok(movie);
+    } catch (MovieNotFoundException | IllegalArgumentException e) {
+      e.printStackTrace();
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @Override
+  public ResponseEntity<Page<Movie>> getMovies(Pageable pageable) {
+    Page<Movie> pagedMovies = movieService.fetchAllMovies(pageable);
+    return ResponseEntity.ok(pagedMovies);
+  }
+
+  @Override
+  @ResponseStatus(HttpStatus.CREATED)
+  public void save(@RequestBody MovieDto movieDto) {
+    Movie movie = Movie.initializeMovie(movieDto);
+    movieService.save(movie);
+  }
 }
