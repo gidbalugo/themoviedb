017142a4b555afd88e839ff0f83fea924846a50c
gidbalugo <zidgyril@gmail.com>
2021-03-07 15:44:27 +0800

    endpoint designs for fetching paginated movies, fetching movie by id, and test endpoint to save movie

--- src/main/java/com/moviedb/api/controller/MovieOperations.java
+++ src/main/java/com/moviedb/api/controller/MovieOperations.java
@@ -1,5 +1,21 @@
 package com.moviedb.api.controller;
 
+import com.moviedb.api.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public interface MovieOperations {
 
+  @GetMapping("/movie/{id}")
+  ResponseEntity<Movie> getById(@PathVariable String id);
+
+  @GetMapping("/movie")
+  ResponseEntity<Page<Movie>> getMovies(Pageable pageable);
+
+  @PostMapping
+  void save(MovieDto movieDto);
 }
